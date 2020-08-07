abe8f1f1e8d4ad5eb9a5ec83bd6f52246d1db45f
Daniel Gatabazi <danielgatabazi@Daniels-MacBook-Air.local>
2020-08-07 10:18:39 -0500

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet("survey")]
+        public IActionResult Result(string Name,string Locations, string Languages, string Comments)
+
+        {
+            ViewBag.Name = Name;
+            ViewBag.Locations = Locations;
+            ViewBag.Languages= Languages;
+            ViewBag.Comments = Comments;
+            return View("Survey");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Survey.cs
@@ -0,0 +1,12 @@
+using System;
+namespace DojoSurveyWithModel.Models
+{
+    public class Survey 
+    {
+        public string Name{get; set;}
+        public string Location {get; set;}
+        public string Languages {get; set;}
+        public string Comments {get; set;}
+    }
+
+}
